d0300a08e6bedda48716f9446b92e8726c6e55c2
San1estus <j.rodriguezisiordia@gmail.com>
2024-01-31 23:34:49 -0600

    Se agregó la función erase

--- EDyA/Tarea1.cpp
+++ EDyA/Tarea1.cpp
@@ -1,4 +1,7 @@
 #include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
 
 template <class TPriority, class TKey>
 class UpdatableHeap {
@@ -10,10 +13,10 @@ private:
     void heapifyUp(int index){
         while(index > 0) {
             int parent = (index - 1) / 2;
-            if(heap[index].priority > heap[parent].priority || (heap[index].priority == heap[parent].priority && heap[index].key > heap[parent].key)){
-                swap(heap[index].priority, heap[parent]);
-                indexMap[heap[index].key] = index;
-                indexMap[heap[parent].key] = index;
+            if(heap[index].first > heap[parent].first || (heap[index].first == heap[parent].first && heap[index].second > heap[parent].second)){
+                swap(heap[index], heap[parent]);
+                indexMap[heap[index].second] = index;
+                indexMap[heap[parent].second] = index;
                 index = parent;
             } else{
                 break;
@@ -26,18 +29,18 @@ private:
         int right = 2 * index + 2;
         int largest = index;
 
-        if(left < heap.size() && (heap[left].priority > heap[largest].priority || (heap[left].priority == heap[largest].priority && heap[left].key > heap[largest].key))){
+        if(left < heap.size() && (heap[left].first > heap[largest].first || (heap[left].first == heap[largest].first && heap[left].second > heap[largest].second))){
             largest = left;
         }
 
-        if(right < heap.size() && (heap[right].priority > heap[largest].priority || (heap[right].priority == heap[largest].priority && heap[right].key > heap[largest].key))){
+        if(right < heap.size() && (heap[right].first > heap[largest].first || (heap[right].first == heap[largest].first && heap[right].second > heap[largest].second))){
             largest = right;
         }
 
         if(largest != index){
             swap(heap[index], heap[largest]);
-            indexMap[heap[index].key] = index;
-            indexMap[heap[largest].key] = largest;
+            indexMap[heap[index].second] = index;
+            indexMap[heap[largest].second] = largest;
             heapifyDown(largest);
         }
     }
@@ -46,7 +49,7 @@ private:
 public:
 
     bool isInserted(const TKey &k) const {
-        return keyIndexMap.find(k) != keyIndexMap.end();
+        return indexMap.find(k) != indexMap.end();
     }
 
     int getSize() const {
@@ -64,20 +67,60 @@ public:
     void pop(){
         indexMap.erase(heap[0].second);
         heap[0] = heap.back();
-        keyIndexMap[heap[0].second] = 0;
+        indexMap[heap[0].second] = 0;
         heap.pop_back();
         heapifyDown(0);
     }
     
-    void erase(const TKey &K) {
+    void insertOrUpdate(const TPriority &p, const TKey &k){
+        if(indexMap.find(k) != indexMap.end()){
+            erase(k);
+        }
+        heap.push_back({p,k});
+        indexMap[k] = (int)heap.size() - 1;
+        heapifyUp((int)heap.size() - 1);
+    }
+
+    void erase(const TKey &k) {
         if(indexMap.find(k) != indexMap.end()){
             int index = indexMap[k];
             int parent = (index - 1)/2;
             indexMap.erase(k);
             heap[index] = heap.back();
-            indexMap[heap[index].key] = index;
+            indexMap[heap[index].second] = index;
             heap.pop_back();
-            
+
+            if(heap[index] > heap[parent]) heapifyUp(index);
+            else heapifyDown(index);
+
         }
     }
 };
+
+int main() {
+    UpdatableHeap<int, string> myHeap;
+    int ops, value;
+    cin >> ops;
+
+    string op, food;
+    while(ops--){
+        cin >> op;
+        if(op == "P"){
+            if(myHeap.isEmpty()) cout << -1 << endl;
+            else cout << myHeap.top().second << " " << myHeap.top().first << endl;
+        }
+        if(op == "IU"){
+            cin >> food >> value;
+            myHeap.insertOrUpdate(value, food);
+        }
+        if(op == "DL"){
+            myHeap.pop();
+        }
+        if(op == "D"){
+            cin >> food;
+            myHeap.erase(food);
+        }
+    }
+
+    return 0;
+}
